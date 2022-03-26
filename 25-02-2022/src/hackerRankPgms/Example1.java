b9017c9ac1912e86f41519b2b44b5eb435115361
Abinaya <abiselvam080@gmail.com>
2022-03-26 11:48:10 +0530

    commit 16

---
+++ 25-02-2022/src/hackerRankPgms/Example1.java
@@ -0,0 +1,16 @@
+package hackerRankPgms;
+import java.util.*;
+public class Example1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the values");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		scan.close();
+	}
+}
